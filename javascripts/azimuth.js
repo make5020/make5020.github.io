8341110e409bcf4a01b66a988e9d9bb72629c85f
win <1605009393@qq.com>
2016-05-13 18:37:40 +0800

    111

---
+++ javascripts/azimuth.js
@@ -0,0 +1,122 @@
+/**
+ * Created by win on 2016/5/12.
+ */
+;(function($, window, document,undefined){
+    var Azimuth=function(ele,options){
+        this.$element =ele;
+        this.defaults ={
+            'speed':'500',
+            'height':$(window).height(),
+            'width':$(window).width(),
+            'backgroundColor': 'white',
+            'auto':true,
+            'start':0,
+            'textAlign':'center'
+        },
+        this.settings = $.extend({},this.defaults, options)
+    }
+    //定义Azimuth的方法
+    Azimuth.prototype={
+        top:function(options){
+          this.$element.on("click", function () {
+              debugger;
+              $('body,html').animate({ scrollTop: 0 }, options.speed);
+          });
+        },
+        bottom:function(options){
+            this.$element.on("click", function () {
+                debugger;
+                $('body,html').animate({ scrollTop: options.height }, options.speed);
+            });
+        },
+        slide:function(options){
+             this.$element.css({'position':'relative','width':options.width,'height':options.height,'background-color':options.backgroundColor,'overflow':'hidden'});
+             var nei_silde =$(this.$element.children()[0]);
+             var $width=size*options.width;
+             nei_silde.css({'position':'absolute','width':$width,'height':options.height,'z-index':100});
+            $(nei_silde.children()).css({'width':options.width,'height':options.height,'float':'left'});
+
+            var size = $(nei_silde.children()).size();
+            var nei_silde_o=$(this.$element.children()[1]);
+            $(nei_silde_o).css({'position':'absolute','width':options.width,'z-index':101,'bottom':0,'text-align':options.textAlign});
+            $(nei_silde_o.children()).css({'width':'8','height':'8','background-color':'#fff','display':'inline-block','border-radius':'8px','margin':'5px'});
+            $(nei_silde_o.children()[options.start]).css({'width':'8','height':'8','background-color':'#333','display':'inline-block','border-radius':'8px','margin':'5px'});
+
+            var left_bar_o=$(this.$element.children()[2]);
+            var bar_h=(options.height/2)-left_bar_o.height()*2;
+            $(left_bar_o).css({'position':'absolute','z-index':101,'background-color':'#fff','top':bar_h,'padding':'30px 10px 30px 0px'});
+            left_bar_o.on("click",function(){
+                options.start++;
+                if(options.start>=size){
+                    options.start=0;
+                }
+                w=options.start*options.width;
+                nei_silde.animate({'left':-w});
+                $(nei_silde_o.children()).css({'width':'8','height':'8','background-color':'#fff','display':'inline-block','border-radius':'8px','margin':'5px'});
+                $(nei_silde_o.children()[options.start]).css({'width':'8','height':'8','background-color':'#333','display':'inline-block','border-radius':'8px','margin':'5px'});
+            })
+
+            var right_bar_o=$(this.$element.children()[3]);
+            $(right_bar_o).css({'position':'absolute','z-index':101,'background-color':'#fff','right':0,'top':bar_h,'padding':'30px 0px 30px 10px'});
+            right_bar_o.on("click",function(){
+                options.start--;
+                if(options.start<0){
+                    options.start=size-1;
+                }
+                w=options.start*options.width;
+                nei_silde.animate({'left':-w});
+                $(nei_silde_o.children()).css({'width':'8','height':'8','background-color':'#fff','display':'inline-block','border-radius':'8px','margin':'5px'});
+                $(nei_silde_o.children()[options.start]).css({'width':'8','height':'8','background-color':'#333','display':'inline-block','border-radius':'8px','margin':'5px'});
+            })
+            if(options.auto){
+                setInterval(function(){
+                    options.start++;
+                    if(options.start>=size){
+                        options.start=0;
+                    }
+                    w=options.start*options.width;
+                    nei_silde.animate({'left':-w});
+                    $(nei_silde_o.children()).css({'width':'8','height':'8','background-color':'#fff','display':'inline-block','border-radius':'8px','margin':'5px'});
+                    $(nei_silde_o.children()[options.start]).css({'width':'8','height':'8','background-color':'#333','display':'inline-block','border-radius':'8px','margin':'5px'});
+                },options.speed);
+            };
+
+            $(this.$element).on("mouseout",function(){
+                right_bar_o.hide();
+                left_bar_o.hide();
+            });
+
+            $(this.$element).on("mousemove",function(){
+                right_bar_o.show();
+                left_bar_o.show();
+            })
+
+            $(nei_silde_o.children()).on("click",function(){
+                var index=$(this).index();
+                console.log(index);
+                options.start=index;
+                w=options.start*options.width;
+                nei_silde.animate({'left':-w});
+                $(nei_silde_o.children()).css({'width':'8','height':'8','background-color':'#fff','display':'inline-block','border-radius':'8px','margin':'5px'});
+                $(nei_silde_o.children()[options.start]).css({'width':'8','height':'8','background-color':'#333','display':'inline-block','border-radius':'8px','margin':'5px'});
+            })
+        }
+
+
+    }
+
+    $.fn.AzimuthTop=function(options){
+        var azimuth=new Azimuth(this,options);
+        return azimuth.top(azimuth.settings);
+    }
+
+    $.fn.AzimuthBottom=function(options){
+        var azimuth=new Azimuth(this,options);
+        return azimuth.bottom(azimuth.settings);
+    }
+
+    $.fn.AzimuthSlide=function(options){
+        var azimuth=new Azimuth(this,options);
+        return azimuth.slide(azimuth.settings);
+    }
+})(jQuery, window, document);
